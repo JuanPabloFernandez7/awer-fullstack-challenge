a6a8f0b5531f0810fd3c76df3d938fee34a530b1
juanpablofer <juanfernandez@itba.edu.ar>
2025-12-16 10:45:08 -0300

    Added database scripts and configuration templates

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO task (title, description) VALUES
+    ('Entrevista inicial', 'Tener entrevista con el equipo Tech.'),
+    ('Iniciar desafio tecnico', 'Completar el desafio tecnico impuesto por el equipo Awer.'),
+    ('Enviar desafio tecnico', 'Terminar el desafio y enviarlo.'),
+    ('Entrar a Awer', 'Si todo sale bien, entrar felizmente a Awer Reviews.');
